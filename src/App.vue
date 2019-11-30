2de2bbbccfd52349bb6b4cb9d5b64265927b3a6a
yangdan <1152362662@qq.com>
2019-11-30 14:30:21 +0800

    [add] code init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Author: yangdan
+ * @Date: 2019-11-30 14:14:02
+ * @LastEditors: yangdan
+ * @LastEditTime: 2019-11-30 14:22:58
+ * @Description: 添加描述
+ -->
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  mounted: () => {
+    let arr = [1, 2, 1];
+    console.log(arr.findIndex(item => item === 1));
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
